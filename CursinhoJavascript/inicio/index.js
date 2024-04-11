c290bd46357f93fcabd97e4c0e31cfa53a685d3d
Guilherme Alcantara <tnt.gamer00125@gmail.com>
2024-04-10 21:56:00 -0300

    ssssssssssdf

---
+++ CursinhoJavascript/inicio/index.js
@@ -0,0 +1,22 @@
+// let count = 0;
+
+// const counter = document.getElementById("counter");
+
+// document.getElementById("id1").onclick = () => {
+//     count++;
+//     counter.textContent = count;
+// };
+
+// document.getElementById("id2").onclick = () => {
+//     count = 0;
+//     counter.textContent = count;
+// };
+
+// document.getElementById("id3").onclick = () => {
+//     count--;
+//     counter.textContent = count;
+// };
+
+// document.getElementById("submit-test").onclick = () => {
+//     document.getElementById("p-test").textContent = document.getElementById("input-test").value;
+// }
