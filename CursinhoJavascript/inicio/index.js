90972d00b8ea2bed8c3b6e90a31397d9d359d6be
Guilherme Alcantara <tnt.gamer00125@gmail.com>
2024-04-18 22:29:53 -0300

    hhmmmm, coisas

--- CursinhoJavascript/inicio/index.js
+++ CursinhoJavascript/inicio/index.js
@@ -1,3 +1,7 @@
+/*
+Counter
+*/
+
 // let count = 0;
 
 // const counter = document.getElementById("counter");
@@ -21,6 +25,10 @@
 //     document.getElementById("p-test").textContent = document.getElementById("input-test").value;
 // }
 
+/*
+Get input
+*/
+
 // const inputAge = document.getElementById("age");
 // const submit = document.getElementById("submit");
 // const result = document.getElementById("result");
@@ -37,6 +45,10 @@
 //         result.textContent = `Já que sua idade é menor que 18, não pode usar este site`;
 //     }
 // };
+
+/*
+Checked
+*/
 
 // const checkbox = document.getElementById("check");
 // const radiochk1 = document.getElementById("radiochk1");
@@ -63,6 +75,10 @@
 //         result2.textContent = ``;
 //     }
 // };
+
+/*
+Adivinhe o número
+*/
 
 // const input = document.getElementById("input");
 // const btn = document.getElementById("submit");
@@ -93,6 +109,10 @@
 //     }
 // };
 
+/*
+Conversor
+*/
+
 // const input = document.getElementById("input");
 // const celsiusF = document.getElementById("temperature1");
 // const fahrenheitC = document.getElementById("temperature2");
@@ -112,6 +132,10 @@
 //     }
 // }
 
+/*
+Spread operator and Rest Parameter
+*/
+
 // const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // const maximum = Math.max(...numbers)
 
@@ -123,17 +147,153 @@
 
 // slkmermao(2, 3, 'sim')
 
-function rollDices() {
-    const dicesNum = Number(document.getElementById("input").value);
-    const result = document.getElementById("result");
-    const imageContainer = document.getElementById("imagesContainer");
-    let dicesValue = [];
-    let diceFaces = [];
-    for (let i = 0; i < dicesNum; i++) {
-        const diceValue = Math.floor(Math.random() * 6 + 1);
-        dicesValue.push(diceValue);
-        diceFaces.push(`<img src="diceFaces/Alea_${diceValue}.png" alt="dice face ${diceValue}">`)
-    }
-    result.innerHTML = `Dados: ${dicesValue.join(", ")}`;
-    imageContainer.innerHTML = diceFaces.join("")
+/* 
+Dice
+*/
+
+// function rollDices() {
+//     const dicesNum = Number(document.getElementById("input").value);
+//     const result = document.getElementById("result");
+//     const imageContainer = document.getElementById("imagesContainer");
+//     let dicesValue = [];
+//     let diceFaces = [];
+//     for (let i = 0; i < dicesNum; i++) {
+//         const diceValue = Math.floor(Math.random() * 6 + 1);
+//         dicesValue.push(diceValue);
+//         diceFaces.push(`<img src="diceFaces/Alea_${diceValue}.png" alt="dice face ${diceValue}">`)
+//     }
+//     result.innerHTML = `Dados: ${dicesValue.join(", ")}`;
+//     imageContainer.innerHTML = diceFaces.join("")
+// }
+
+/*
+Random Password
+*/
+
+// function passGen(length = 10, low = true, upper = false, nums = false, symbols = false) {
+//     const lowS = "abcdefghijklmnopqrstuvwxyz";
+//     const upperS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//     const numsS = "0123456789";
+//     const symbolsS = "!@#$%^&*()_+-=";
+
+//     let tempPass = "";
+//     let password = "";
+
+//     tempPass += low ? lowS : "";
+//     tempPass += upper ? upperS : "";
+//     tempPass += nums ? numsS : "";
+//     tempPass += symbols ? symbolsS : "";
+
+//     if (length <= 0) {
+//         console.log("O tamanho deve ser maior ou igual a 1");
+//     } else if (tempPass.length === 0) {
+//         console.log("Tem que ter pelo menos um tipo de chat selecionado");
+//     } else {
+//         for (let i = 0; i < length; i++) {
+//             const randomChar = Math.floor(Math.random() * tempPass.length);
+//             const char = tempPass[randomChar];
+//             password += char;
+//         }
+//     }
+
+//     return password;
+// }
+
+// console.log(passGen(10, true, true, true, true))
+
+/*
+Callbacks
+ */
+
+// function hello(callback) {
+//     console.log("slk mermao eae mano");
+//     callback();
+// }
+
+// function bye() {
+//     console.log("olha a hora maluco, flw ae");
+// }
+
+// hello(bye);
+
+/*
+ forEach 
+ */
+
+// function print(value, index, array) {
+//     console.log(value);
+//     console.log(index);
+//     console.log(array);
+// }
+
+// const myArray = [1, 2, 3, 4, 4, 5, 5, 6, 6, 7, 7];
+
+// myArray.forEach(print);
+
+/*
+map
+*/
+
+// const myArray = [1, 2, 3, 4, 4, 5, 5, 6, 6, 7, 7];
+
+// function double(value, index, array) {
+//     return (array[index] = value * 2);
+// }
+
+// let doubles = myArray.map(double);
+
+/**
+ * filter
+ */
+
+// const myArray = [1, 2, 3, 4, 4, 5, 5, 6, 6, 7, 7];
+
+// function isEven(element) {
+//         return element % 2 === 0;
+//     }
+
+//     const noon = myArray.filter(isEven);
+
+/**
+ * reduce
+ */
+
+// const myArray = [1, 2, 3, 4, 4, 5, 8, 6, 6, 7, 7];
+
+// const total = myArray.reduce((a,b) => a + b, 0);
+
+// const max = myArray.reduce((a,b) => Math.max(a,b), 0)
+
+/**
+ * Objects
+ */
+
+const person = {
+    name: "Aroldo",
+    lastName: "Jorge",
+    age: 86,
+    isEmployed: false,
+    funFact: "Aroldo ama Aroldo Jorge (É ele mesmo))",
+    saySomething: (thing = "aha") => {
+        console.log(`OIiiiiii eu sou o Aroldo Jorge, ${thing}`);
+    },
+    saySomethingThis: function (thing = "aha") {
+        console.log(
+            `OIiiiiii eu sou o ${this.name} ${this.lastName}, ${thing}`
+        ); // This só funciona com a palavra function e não com arrow functions
+    },
+};
+
+person.saySomething(":OOOO");
+person.saySomethingThis(":OOOOOO");
+
+// Construtores
+
+function car(make, model, year, color) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.color = color;
 }
+
+const camaro = new car('Chevrolet', 'Camaro', 2000, 'yellow');
