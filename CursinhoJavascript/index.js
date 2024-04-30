53700d72a9f34645a08750aa1c4ee9fd7656782a
Guilherme Alcantara <tnt.gamer00125@gmail.com>
2024-04-30 16:59:09 -0300

    aaa

--- CursinhoJavascript/index.js
+++ CursinhoJavascript/index.js
@@ -547,15 +547,16 @@ map
  * relógio
  */
 
-function clock() {
-    const date = new Date();
-    let hours = date.getHours().toString().padStart(2, 0);
-    let minutes = date.getMinutes().toString().padStart(2, 0);
-    let seconds = date.getSeconds().toString().padStart(2, 0);
-    const time = `${hours}:${minutes}:${seconds}`
-    document.getElementById('clock').innerHTML = `<span>${time}</span>`
-}
+// function clock() {
+//     const date = new Date();
+//     let hours = date.getHours().toString().padStart(2, 0);
+//     let minutes = date.getMinutes().toString().padStart(2, 0);
+//     let seconds = date.getSeconds().toString().padStart(2, 0);
+//     const time = `${hours}:${minutes}:${seconds}`
+//     document.getElementById('clock').innerHTML = `<span>${time}</span>`
+// }
 
-clock()
+// clock()
 
-setInterval(clock, 500)
+// setInterval(clock, 500)
+
