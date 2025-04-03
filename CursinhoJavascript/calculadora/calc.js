9770b11b76a32f04cf6e020b554dcd52cb2181cf
Guilherme Alcantara <tnt.gamer00125@gmail.com>
2025-04-03 17:29:44 -0300

    calc update

--- CursinhoJavascript/calculadora/calc.js
+++ CursinhoJavascript/calculadora/calc.js
@@ -1,27 +1,51 @@
 const display = document.getElementById('calculus');
+let lastAns = 0;
 
-function adjustSize() {
-	const textLength = display.value.length;
-    display.style.fontSize = `${64 - (textLength * 1)}px`
+function adjustFontSize() {
+    display.style.fontSize =
+        Math.max(8 - Math.max(display.value.length - 8, 0) * 0.7325, 3) +
+        'vmin';
 }
 
 function calcAppend(value) {
-	display.value += value;
-    adjustSize()
+    display.value += value;
+    adjustFontSize();
 }
 
 function clearDisplay() {
-	display.value = '';
-    adjustSize()
+    display.value = '';
+    adjustFontSize();
+}
+
+function backspace() {
+    display.value = display.value.slice(0, -1);
+    adjustFontSize();
 }
 
 function calc() {
-	try {
-		display.value = eval(display.value);
-        adjustSize()
-	} catch (error) {
-		display.value = 'Error';
-        adjustSize()
-		setTimeout(clearDisplay, 1000);
-	}
+    try {
+        let expression = display.value;
+        expression = expression.replace(/\^/g, '**');
+        expression = expression.replace(/Ans/g, lastAns);
+        expression = expression.replace(/π/g, 'Math.PI');
+        expression = expression.replace(/√(\d+)|√(\(.+\))/g, 'Math.sqrt($1$2)');
+        console.log(expression);
+
+        const result = eval(expression);
+        lastAns = result;
+        if (result === undefined || result === null || isNaN(result)) {
+            throw new Error('Erro');
+        } else if (result === Infinity) {
+            throw new Error('Divisão por zero');
+        }
+        display.value = result;
+    } catch (error) {
+        display.value = error.message.includes('zero')
+            ? 'Não é possível dividir por zero'
+            : 'Erro de sintaxe (provavelmente)';
+        console.error(error);
+        setTimeout(clearDisplay, 1500);
+    } finally {
+        adjustFontSize();
+    }
 }
